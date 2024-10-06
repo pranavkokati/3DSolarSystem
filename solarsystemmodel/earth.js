f4a64b5972a89ba66d31199465dea4fa52f82f4e
Pranav Kokati <pranav.kokati@gmail.com>
2024-10-05 19:55:53 -0700

    Delete solarsystemmodel directory

--- solarsystemmodel/earth.js
+++
@@ -1,8 +0,0 @@
-// Create Earth
-const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
-const earthMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const earth = new THREE.Mesh(earthGeometry, earthMaterial);
-
-// Place Earth at distance from Sun
-earth.position.x = 20;
-scene.add(earth);
--- solarsystemmodel/sun.js
+++
@@ -1,5 +0,0 @@
-// Create Sun
-const sunGeometry = new THREE.SphereGeometry(5, 32, 32);
-const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-const sun = new THREE.Mesh(sunGeometry, sunMaterial);
-scene.add(sun);
