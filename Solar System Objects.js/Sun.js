4fc39ff63d514d2dae997d145b651ee3655ddbb4
Pranav Kokati <pranav.kokati@gmail.com>
2024-10-05 20:12:35 -0700

    Create Sun.js

---
+++ Solar System Objects.js/Sun.js
@@ -0,0 +1,5 @@
+// Create the Sun
+const sunGeometry = new THREE.SphereGeometry(5, 32, 32);
+const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffcc00 });
+const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+scene.add(sun);
